470047b8a8cd69d4c034615c07c7e1e35183eb43
salman <salmankhan.faze@gmail.com>
2023-11-01 21:03:33 +0530

    Adding Contact US Page via REACT ROUTER

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Store from './Store'
 import About from './About'
 import Home from './Home'
+import Contact from './Contact'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 const router = createBrowserRouter([
   {path:'/',element: <Store />},
   {path:'/about',element: <About />},
-  {path: '/home',element: <Home />}
+  {path: '/home',element: <Home />},
+  {path: '/contact',element: <Contact />}
 ])
 function App() {
   return (
